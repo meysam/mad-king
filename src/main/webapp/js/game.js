e8f88a0fa1dc2947adad484791794d8a1ecad43e
meysam <meysam.tamkin@gmail.com>
2017-08-07 01:59:05 +0430

    Changed fucking style this game
    Added jquery lib for controling elements

--- src/main/webapp/js/game.js
+++ src/main/webapp/js/game.js
@@ -1,196 +1,227 @@
 /**
  * Created by meysam on 8/6/17.
  */
-var webSocket;
-var output = document.getElementById("output");
-var connectBtn = document.getElementById("connectBtn");
-var sendBtn = document.getElementById("sendBtn");
-var url = "ws://localhost:8080/madKing";
 
-// Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
+$(document).ready(function () {
 
-// Background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-    bgReady = true;
-};
-bgImage.src = "images/background.png";
+    $(".StartButton").click(function () {
+        $(".SplashScreen").hide();
+        $(".FinishScreen").hide();
+        $("#canvasArea").show();
+        clearInterval(endGameInterval);
+        init();
+    });
 
-// Mad King image
-var madKingReady = false;
-var madKingImage = new Image();
-madKingImage.onload = function () {
-    madKingReady = true;
-};
-madKingImage.src = "images/madKing.png";
-
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-    heroReady = true;
-};
-heroImage.src = "images/hero.png";
+    var webSocket;
+    var output = document.getElementById("output");
+    var connectBtn = document.getElementById("connectBtn");
+    var sendBtn = document.getElementById("sendBtn");
+    var url = "ws://localhost:8080/madKing";
+    var timeleft = 60;
+    var endGameInterval;
 
 // Game objects
-var madKing = {
-    speed: 256 // movement in pixels per second
-};
-var hero = {};
-var herosCaught = 0;
-
+    var madKing = {
+        speed: 256 // movement in pixels per second
+    };
+    var hero = {};
+    var herosCaught = 0;
 // Handle keyboard controls
-var keysDown = {};
+    var keysDown = {};
+//Canvas stuff
+    var canvas = $("#canvasArea")[0];
+    var ctx = canvas.getContext("2d");
+    var w = $("#canvasArea").width();
+    var h = $("#canvasArea").height();
 
-addEventListener("keydown", function (e) {
-    keysDown[e.keyCode] = true;
-}, false);
+// Background image
+    var bgReady = false;
+    var bgImage = new Image();
+// Mad King image
+    var madKingReady = false;
+    var madKingImage = new Image();
+// Hero image
+    var heroReady = false;
+    var heroImage = new Image();
+    var then = Date.now();
 
-addEventListener("keyup", function (e) {
-    delete keysDown[e.keyCode];
-}, false);
+    function init() {
+        herosCaught = 0;
+        bgImage.onload = function () {
+            bgReady = true;
+        };
+        bgImage.src = "images/background.png";
 
-// Reset the game when the player catches a hero
-var reset = function () {
-    madKing.x = canvas.width / 2;
-    madKing.y = canvas.height / 2;
 
-    // Throw the hero somewhere on the screen randomly
-    hero.x = 32 + (Math.random() * (canvas.width - 64));
-    hero.y = 32 + (Math.random() * (canvas.height - 64));
-};
+        madKingImage.onload = function () {
+            madKingReady = true;
+        };
+        madKingImage.src = "images/madKing.png";
 
-// Update game objects
-var update = function (modifier) {
-    if (38 in keysDown) { // Player holding up
-        madKing.y -= madKing.speed * modifier;
-    }
-    if (40 in keysDown) { // Player holding down
-        madKing.y += madKing.speed * modifier;
-    }
-    if (37 in keysDown) { // Player holding left
-        madKing.x -= madKing.speed * modifier;
-    }
-    if (39 in keysDown) { // Player holding right
-        madKing.x += madKing.speed * modifier;
-    }
 
-    // Are they touching?
-    if (
-        madKing.x <= (hero.x + 32)
-        && hero.x <= (madKing.x + 32)
-        && madKing.y <= (hero.y + 32)
-        && hero.y <= (madKing.y + 32)
-    ) {
-        ++herosCaught;
-        reset();
-    }
-};
+        heroImage.onload = function () {
+            heroReady = true;
+        };
+        heroImage.src = "images/hero.png";
 
-// Draw everything
-var render = function () {
-    if (bgReady) {
-        ctx.drawImage(bgImage, 0, 0);
-    }
 
-    if (madKingReady) {
-        ctx.drawImage(madKingImage, madKing.x, madKing.y);
-    }
+        addEventListener("keydown", function (e) {
+            keysDown[e.keyCode] = true;
+        }, false);
 
-    if (heroReady) {
-        ctx.drawImage(heroImage, hero.x, hero.y);
-    }
+        addEventListener("keyup", function (e) {
+            delete keysDown[e.keyCode];
+        }, false);
 
-    // Score
-    ctx.fillStyle = "rgb(250, 250, 250)";
-    ctx.font = "24px Helvetica";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Goblins caught: " + herosCaught, 32, 32);
-};
-
-// The main game loop
-var main = function () {
-    var now = Date.now();
-    var delta = now - then;
-
-    update(delta / 1000);
-    render();
-
-    then = now;
-
-    // Request to do this again ASAP
-    requestAnimationFrame(main);
-};
-
-// Cross-browser support for requestAnimationFrame
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+        // Cross-browser support for requestAnimationFrame
+        var w = window;
+        requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 // Let's play this game!
-var then = Date.now();
-reset();
-main();
 
-function connect() {
-    // open the connection if one does not exist
-    if (webSocket !== undefined
-        && webSocket.readyState !== WebSocket.CLOSED) {
-        return;
+        reset();
+        main();
     }
-    // Create a websocket
-    webSocket = new WebSocket(url);
 
-    webSocket.onopen = function (event) {
-        updateOutputText("Connected!");
-        connectBtn.disabled = true;
-        sendBtn.disabled = false;
+    // Reset the game when the player catches a hero
+    var reset = function () {
+        endGameInterval = setInterval(function () {
+            endGame()
+        }, 5000);
+        madKing.x = canvas.width / 2;
+        madKing.y = canvas.height / 2;
 
+        // Throw the hero somewhere on the screen randomly
+        hero.x = 32 + (Math.random() * (canvas.width - 64));
+        hero.y = 32 + (Math.random() * (canvas.height - 64));
     };
 
-    webSocket.onmessage = function (event) {
-        updateOutput(event.data);
+// Update game objects
+    var update = function (modifier) {
+        if (38 in keysDown) { // Player holding up
+            madKing.y -= madKing.speed * modifier;
+        }
+        if (40 in keysDown) { // Player holding down
+            madKing.y += madKing.speed * modifier;
+        }
+        if (37 in keysDown) { // Player holding left
+            madKing.x -= madKing.speed * modifier;
+        }
+        if (39 in keysDown) { // Player holding right
+            madKing.x += madKing.speed * modifier;
+        }
+
+        // Are they touching?
+        if (
+            madKing.x <= (hero.x + 32)
+            && hero.x <= (madKing.x + 32)
+            && madKing.y <= (hero.y + 32)
+            && hero.y <= (madKing.y + 32)
+        ) {
+            clearInterval(endGameInterval);
+            ++herosCaught;
+            reset();
+        }
     };
 
-    webSocket.onclose = function (event) {
-        updateOutputText("Connection Closed");
-        connectBtn.disabled = false;
-        sendBtn.disabled = true;
+// Draw everything
+    var render = function () {
+        if (bgReady) {
+            ctx.drawImage(bgImage, 0, 0);
+        }
+
+        if (madKingReady) {
+            ctx.drawImage(madKingImage, madKing.x, madKing.y);
+        }
+
+        if (heroReady) {
+            ctx.drawImage(heroImage, hero.x, hero.y);
+        }
+
+        // Score
+        ctx.fillStyle = "rgb(250, 250, 250)";
+        ctx.font = "24px Helvetica";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+        ctx.fillText("Hero caught: " + herosCaught, 32, 32);
     };
-}
 
-function send() {
-    var text = document.getElementById("input").value;
-    var uid = document.getElementById("uid").value;
-    var message = '{'
-        + '"msg" : "' + text + '",'
-        + '"uniqueId"  : "' + uid + '"'
-        + '}';
-    webSocket.send(message);
-}
+// The main game loop
+    var main = function () {
+        var now = Date.now();
+        var delta = now - then;
 
-function closeSocket() {
-    webSocket.close();
-}
+        update(delta / 1000);
+        render();
 
-function updateOutputText(text) {
-    document.getElementById("output").value += "\n" + text;
-}
+        then = now;
+        // endGame();
+        requestAnimationFrame(main);
+    };
 
-function updateOutput(text) {
+    function endGame() {
+        $("#canvasArea").hide();
+        $("#score").text(herosCaught);
+        $(".FinishScreen").show();
+    }
 
-    var obj = JSON.parse(text, function (key, value) {
-        if (key == "uniqueId") {
-            document.getElementById("uid").value = value;
-            return value;
-        } else {
-            return value;
-        }});
-    document.getElementById("output").value += "\n" + text;
-}
+    function connect() {
+        // open the connection if one does not exist
+        if (webSocket !== undefined
+            && webSocket.readyState !== WebSocket.CLOSED) {
+            return;
+        }
+        // Create a websocket
+        webSocket = new WebSocket(url);
+
+        webSocket.onopen = function (event) {
+            updateOutputText("Connected!");
+            connectBtn.disabled = true;
+            sendBtn.disabled = false;
+
+        };
+
+        webSocket.onmessage = function (event) {
+            updateOutput(event.data);
+        };
+
+        webSocket.onclose = function (event) {
+            updateOutputText("Connection Closed");
+            connectBtn.disabled = false;
+            sendBtn.disabled = true;
+        };
+    }
+
+    function send() {
+        var text = document.getElementById("input").value;
+        var uid = document.getElementById("uid").value;
+        var message = '{'
+            + '"msg" : "' + text + '",'
+            + '"uniqueId"  : "' + uid + '"'
+            + '}';
+        webSocket.send(message);
+    }
+
+    function closeSocket() {
+        webSocket.close();
+    }
+
+    function updateOutputText(text) {
+        document.getElementById("output").value += "\n" + text;
+    }
+
+    function updateOutput(text) {
+
+        var obj = JSON.parse(text, function (key, value) {
+            if (key == "uniqueId") {
+                document.getElementById("uid").value = value;
+                return value;
+            } else {
+                return value;
+            }
+        });
+        document.getElementById("output").value += "\n" + text;
+    }
+
+
+});
